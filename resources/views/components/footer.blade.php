a276891622ff538b1504e206f5bb288d1a563393
uoslau <221911080@stis.ac.id>
2025-08-04 23:08:11 +0700

    hapus footer yang ga perlu

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,22 +2,7 @@
     <div class="container-xxl">
         <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
             <div class="text-body">
-                ©
-                <script>
-                    document.write(new Date().getFullYear());
-                </script>
-                , made with ❤️ by
-                <a href="https://themeselection.com" target="_blank" class="footer-link">ThemeSelection</a>
-            </div>
-            <div class="d-none d-lg-inline-block">
-                <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-
-                <a href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
-                    target="_blank" class="footer-link me-4">Documentation</a>
-
-                <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
-                    class="footer-link">Support</a>
+                © 2024, made with ☕ by BPS Kabupaten Nias
             </div>
         </div>
     </div>
